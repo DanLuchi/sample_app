ab6e4de0c5610cad9c7820e23ed4f1c53d7ede68
DanLuchi <dan@danluchi.com>
2011-01-14 11:31:20 -0800

    Finished converting from cookies to sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,15 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    # Session sign in - signed out when browser closes
+    session[:user] = user.id 
+
+    # Permanent cookie sign in - not signed out when browser closes
+    # cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+
+    # A session will end when the browser is closed if 'permanent' is not used
+    # cookies.signed[:remember_token] = [user.id, user.salt]
+
     self.current_user = user
   end
 
@@ -10,7 +18,11 @@ module SessionsHelper
   end
 
   def current_user
-    @curent_user ||= user_from_remember_token
+    # For sessions
+    @current_user ||= User.find_by_id(session[:user])    
+
+    # For cookies
+    # @curent_user ||= user_from_remember_token
   end
 
   def signed_in?
@@ -18,7 +30,11 @@ module SessionsHelper
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    # Session delete 
+    session.delete(:user)
+
+    # Cookie delete
+    # cookies.delete(:remember_token)
     self.current_user = nil
   end
 
