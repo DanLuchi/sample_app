56a69c4dbc7e7e3ad209cc8291ea313a156a29e7
DanLuchi <dan@danluchi.com>
2011-01-17 19:53:02 -0800

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,24 @@
 # == Schema Information
-# Schema version: 20110110004857
+# Schema version: 20110116234112
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean
 #
 
 require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation 
+
+  has_many :microposts, :dependent => :destroy
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -29,6 +34,10 @@ class User < ActiveRecord::Base
 		       :length       => { :within => 6..40 }
 
   before_save :encrypt_password
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
